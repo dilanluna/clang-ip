65e5bd5d3aff2eff142fc0e2acc383449f4024e4
Dylan Luna <me@dilanluna.com>
2022-10-20 21:06:18 -0600

    feat: calc broadcast ip

--- main.c
+++ main.c
@@ -14,20 +14,25 @@ uint32_t str_ip_to_decm(char *);
 
 void decm_ip_to_str(uint32_t, char *);
 
-uint32_t network_ip(uint32_t, uint32_t);
+uint32_t net_ip(uint32_t, uint32_t);
+
+uint32_t brdcst_ip(uint32_t, uint32_t);
 
 int main() {
-  char net[16];
-  char host[16] = "192.168.1.1";
-  char mask[16] = "255.255.255.0";
+  char str_net[16];
+  char str_brdcst[16];
+  char str_host[16] = "192.168.1.1";
+  char str_mask[16] = "255.255.255.0";
   
-  uint32_t decm_host = str_ip_to_decm(host);
-  uint32_t decm_mask = str_ip_to_decm(mask);
-  uint32_t decm_net = network_ip(decm_host, decm_mask);
+  uint32_t decm_host = str_ip_to_decm(str_host);
+  uint32_t decm_mask = str_ip_to_decm(str_mask);
+  uint32_t decm_net = net_ip(decm_host, decm_mask);
+  uint32_t decm_brdcst = brdcst_ip(decm_host, decm_mask);
   
-  decm_ip_to_str(decm_net, net);
-  printf("%u\n", decm_net);
-  printf("%s\n", net);
+  decm_ip_to_str(decm_net, str_net);
+  decm_ip_to_str(decm_brdcst, str_brdcst);
+  printf("Network IP: %s\n", str_net);
+  printf("Broadcast IP: %s\n", str_brdcst);
 
   return 0;
 }
@@ -59,6 +64,11 @@ void decm_ip_to_str(uint32_t decm_ip, char *str_ip) {
   sprintf(str_ip, "%u.%u.%u.%u", ip.octs[3], ip.octs[2], ip.octs[1], ip.octs[0]);
 }
 
-uint32_t network_ip(uint32_t decm_host, uint32_t decm_mask) {
+uint32_t net_ip(uint32_t decm_host, uint32_t decm_mask) {
   return decm_host & decm_mask;
+}
+
+uint32_t brdcst_ip(uint32_t decm_host, uint32_t decm_mask) {
+	uint32_t decm_brdcst_mask = ~decm_mask;
+	return decm_host | decm_brdcst_mask;
 }
